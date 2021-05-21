d73fc9ffb1ab51ff8a7cbeb850bb4346194bca12
Diego Sarmiento <diegosarmientoq1245@gmail.com>
2021-05-20 21:40:40 -0500

    globlal scope file

---
+++ src/scope/global.js
@@ -0,0 +1,24 @@
+// Global scope
+
+// Are accesible throughout the program. A variable declared outside a function. it means that all scripts and functions on a webpage can access it. 
+
+// Only Var can be reassigned
+
+var diego = 'abogado'
+var diego = 'developer'
+
+console.log(diego)
+
+// You can acces to global scope when variables are declared globally. But you can't redeclared Let or Const values.
+
+var hola = 'holi'
+let hola2 = 'hola'
+const hola3 = 'holanda'
+
+const myFunction = () => {
+    console.log(hola)
+    console.log(hola2)
+    console.log(hola3)
+}
+myFunction();
+
